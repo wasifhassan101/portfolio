395cc74bdd57fd1af8a6d176c45a7cbe8558d590
Wasif Hassan <wasifhassan101@gmail.com>
2023-09-15 18:00:29 +0500

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,58 @@
 import Image from 'next/image'
+import wasif from '../public/wasif.jpg'
+import ls from "../public/lawscript.png"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='bg-black'>
+      <div className='relative h-full flex flex-col p-10'>
+        <h1 className='text-yellow text-5xl mt-20 font-roboto w-1/3'>
+          Wasif Hassan — Software Developer
+        </h1>
+        <div className='w-full flex '>
+          <div className='w-1/3 translate-y-10 left-10'>
+            <Image src={wasif} />
+          </div>
+          <p className='w-2/3 p-32 font-roboto-light text-white text-lg'>
+            Hi, I'm Wasif. I'm a software developer with a unique blend of expertise in law and technology.
+            While I began my career as a law student, my passion for coding led me to found LawScript, a startup that
+            leverages AI to revolutionize legal research and writing in Pakistan. This venture serves as a testament to
+            my ability to innovate and lead in the tech space.
+            <br />
+            <br />
+            Although LawScript is one of my key projects, my interests are not confined to legal tech.
+            I'm currently diving into the realms of AI and machine learning, aspiring to broaden my skill set
+            and contribute to various sectors. Specializing in software development, I have the versatility and
+            dedication to work on complex projects, from development to deployment.
+            <br />
+            <br />
+            If you're looking for a committed professional who can elevate your technological solutions
+            and has the capability to work in unison with multiple teams, I'm the person you're looking for.
+            Let's connect to explore how we can collaborate on your next project.
+          </p>
         </div>
+      </div >
+      <div className='flex items-center flex-col bg-light-gray p-32 space-y-10'>
+        <h1 className='text-black text-4xl font-roboto-light'>
+          <span className='font-roboto'>LawScript</span> — revolutionizing the legal landscape with AI-driven research and writing tools
+          that blend efficiency, innovation, and precision.
+        </h1>
+        <div className='w-4/5'>
+          <Image className='rounded-2xl' src={ls} />
+        </div>
+        <p className='font-roboto-light text-black text-lg text-justify'>
+          As the CTO and co-founder of LawScript, I led the technical development of the platform
+          from its inception on March 15 to its successful launch on September 11. Within this period,
+          I faced and overcame a myriad of technical challenges, including the rapid development of the
+          website in just one month. I made pivotal decisions on database architecture for both user data
+          and AI models, and meticulously selected the most suitable AI algorithms for our legal research 
+          tools. I also managed the complex task of structuring, parsing, and cleaning caselaw and statutes,
+          resolving intricate issues such as formatting and structural errors in those documents. While my 
+          primary role has been technical, I've collaborated closely with my co-founder to shape the business
+          strategy, ensuring a well-rounded approach to our startup's growth. As of the launch, I have transitioned
+          to building a team to further enhance and scale our platform.
+        </p>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </main >
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundColor: {
+        'light-gray': '#B1B1B1',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'yellow': '#f1f952'
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'roboto-light': ['RobotoLight', 'sans-serif'],
+        'roboto-bold': ['RobotoBold', 'sans-serif'],
       },
     },
   },
