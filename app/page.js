187303b8d72cec307d48fcf0b986a22580499809
Wasif Hassan <wasifhassan101@gmail.com>
2023-10-26 20:18:18 +0500

    edited

--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
 import Image from 'next/image'
-import wasif from '../public/wasif.jpg'
+import wasif from '../public/wasif1.png'
 import ls from "../public/lawscript.png"
+import threads from "../public/threads.svg"
+import gh from "../public/github.svg"
+import ig from "../public/instagram.svg"
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,11 +13,11 @@ export default function Home() {
         <h1 className='text-yellow text-5xl mt-20 font-roboto w-1/3'>
           Wasif Hassan — Software Developer
         </h1>
-        <div className='w-full flex '>
+        <div className='w-full flex'>
           <div className='w-1/3 translate-y-10 left-10'>
             <Image src={wasif} />
           </div>
-          <p className='w-2/3 p-32 font-roboto-light text-white text-lg'>
+          <p className='w-2/3 p-12 font-roboto-light text-white text-lg'>
             Hi, I'm Wasif. I'm a software developer with a unique blend of expertise in law and technology.
             While I began my career as a law student, my passion for coding led me to found LawScript, a startup that
             leverages AI to revolutionize legal research and writing in Pakistan. This venture serves as a testament to
@@ -45,13 +49,33 @@ export default function Home() {
           from its inception on March 15 to its successful launch on September 11. Within this period,
           I faced and overcame a myriad of technical challenges, including the rapid development of the
           website in just one month. I made pivotal decisions on database architecture for both user data
-          and AI models, and meticulously selected the most suitable AI algorithms for our legal research 
+          and AI models, and meticulously selected the most suitable AI algorithms for our legal research
           tools. I also managed the complex task of structuring, parsing, and cleaning caselaw and statutes,
-          resolving intricate issues such as formatting and structural errors in those documents. While my 
+          resolving intricate issues such as formatting and structural errors in those documents. While my
           primary role has been technical, I've collaborated closely with my co-founder to shape the business
           strategy, ensuring a well-rounded approach to our startup's growth. As of the launch, I have transitioned
           to building a team to further enhance and scale our platform.
         </p>
+      </div>
+      <div className='bg-black p-20'>
+        <h1 className='text-yellow text-3xl font-roboto'>Let's Connect!</h1>
+        <div className='flex items-center h-20'>
+          <Link target='_blank' href="https://github.com/wasifhassan101">
+            <div className='w-12'>
+              <Image src={gh} className='w-10 hover:w-12' />
+            </div>
+          </Link>
+          <Link target='_blank' href='https://www.threads.net/@wasif_codes'>
+            <div className='w-12'>
+              <Image src={threads} className='w-7 hover:w-9' />
+            </div>
+          </Link>
+          <Link target='_blank' href='https://instagram.com/wasif_codes?igshid=NGVhN2U2NjQ0Yg%3D%3D&utm_source=qr'>
+            <div className='w-12'>
+              <Image src={ig} className='w-7 hover:w-9' />
+            </div>
+          </Link>
+        </div>
       </div>
     </main >
   )
